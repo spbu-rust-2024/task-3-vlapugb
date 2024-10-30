9c0cacf2d5ad6b8affe4032400735ee0dda1ff25
vlapugb <vladimirpugovkinspb@gmail.com>
2024-10-30 13:10:47 +0300

    style: del debug info from kolmog

--- src/modules/kolmogorov_smirnov.rs
+++ src/modules/kolmogorov_smirnov.rs
@@ -1,12 +1,10 @@
 use std::f64::consts::E;
 
 fn f(x: f64) -> f64 {
-    println!("{}", E.powf(x) + 2.0 * x * x + x + 5.0);
     E.powf(x) + 2.0 * x * x + x + 5.0
 }
 
 fn df(x: f64) -> f64 {
-    println!("{}", E.powf(x) + 4.0 * x + 1.0);
     E.powf(x) + 4.0 * x + 1.0
 }
 
@@ -31,6 +29,5 @@ pub fn kolmogorov_smirnov(vec: &Vec<f64>) -> f64 {
         .map(|&x| E.powf(x) + 2.0 * x * x + x + 5.0)
         .sum::<f64>()
         / vec.len() as f64;
-    println!("x: {} ", &x);
     newton_method(x)
 }
