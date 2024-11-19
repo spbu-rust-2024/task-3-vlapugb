6e5b278966eccea603dd653c6cb1590a31a93cd7
vlapugb <vladimirpugovkinsbp@gmail.com>
2024-11-20 02:25:17 +0300

    refarctor: mv tests to fns

--- src/modules/arifmetic_mean.rs
+++ src/modules/arifmetic_mean.rs
@@ -7,3 +7,33 @@ pub fn arifmetic_mean(vec: &Vec<f64>) -> f64 {
     let sum: f64 = vec.iter().sum();
     return sum / vec.len() as f64;
 }
+
+#[cfg(test)]
+#[allow(unused_variables)]
+mod tests {
+    use arrayp::modules::calculations::arifmetic_mean;
+    use rstest::rstest;
+    #[rstest]
+    #[case(&[1.0, 2.0, 3.0], 2.0)]
+    #[case(&[1.0, 2.0, 3.0, 99.5], 26.375)]
+    fn test_arifmetic_mean_all_numbers_positive(#[case] test_vec: &[f64], #[case] expected: f64) {
+        let test_vec: Vec<f64> = Vec::from(test_vec);
+        let result = arifmetic_mean(&test_vec);
+        assert_eq!(
+            (result * 1e10).round() / 1e10,
+            (expected * 1e10).round() / 1e10
+        );
+    }
+    #[test]
+    fn test_arifmetic_mean_with_negative_numbers() {
+        let test_vec: Vec<f64> = Vec::from([1.0, -2.0, 3.0, -4.0, 5.0]);
+        let result = arifmetic_mean(&test_vec);
+        assert_eq!(result, 0.6);
+    }
+    #[test]
+    #[should_panic]
+    fn test_arifmetic_mean_null_elements() {
+        let test_vec: Vec<f64> = Vec::from([]);
+        let result = arifmetic_mean(&test_vec);
+    }
+}
--- src/modules/power_mean.rs
+++ src/modules/power_mean.rs
@@ -6,3 +6,37 @@ pub fn power_mean(vec: &Vec<f64>, n: &u16) -> f64 {
     println!("{}", powered);
     return (powered / vec.len() as f64).powf(1.0 / *n as f64);
 }
+
+
+#[cfg(test)]
+#[allow(unused_variables)]
+mod tests {
+    use arrayp::modules::calculations::power_mean;
+    use rstest::rstest;
+    #[rstest]
+    #[case(&[2.0, 2.0, 2.0, 2.0, 2.0], 2.0, 2)]
+    #[case(&[1.0, 1.0, 1.0, 1.0, 1.0], 1.0, 3)]
+    #[case(&[3.0, 3.0, 3.0], 3.0, 1)]
+    fn test_power_mean(#[case] test_vec: &[f64], #[case] expected: f64, #[case] power: u16) {
+        let test_vec: Vec<f64> = Vec::from(test_vec);
+        let result = power_mean(&test_vec, &power);
+        assert_eq!(
+            (result * 1e10).round() / 1e10,
+            (expected * 1e10).round() / 1e10
+        );
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_power_mean_null_elements() {
+        let test_vec: Vec<f64> = Vec::from([]);
+        let result = power_mean(&test_vec, &4);
+    }
+    #[test]
+    #[should_panic]
+    fn test_power_mean_invalid_power() {
+        let test_vec: Vec<f64> = Vec::from([]);
+        let result = power_mean(&test_vec, &6);
+    }
+}
+
